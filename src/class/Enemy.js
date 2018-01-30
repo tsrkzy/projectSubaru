0ccb4d8f84be0ac34025f2846680dbe3f58e65ec
k.tashiro <tashiro@lepra.jp>
2018-01-30 21:58:56 +0900

    impl: waveConfig

--- src/class/Enemy.js
+++ src/class/Enemy.js
@@ -12,7 +12,7 @@ class Enemy {
   get hitPoint() {
     return this._hitPoint;
   }
-  
+
   set hitPoint(value) {
     this._hitPoint = value;
     if (this._hitPoint <= 0) {
@@ -20,7 +20,7 @@ class Enemy {
       this.destroyed();
     }
   }
-  
+
   /**
    * @constructor
    * @param {Object} args - x, y, stage
@@ -28,17 +28,18 @@ class Enemy {
   constructor(args) {
     this.id = Enemy.id;
     Enemy.id++;
-    this.x        = args.x;
-    this.y        = args.y;
+    this.x = args.x;
+    this.y = args.y;
     this.airCraft = AirCraft.getInstance();
     this.hitPoint = args.hitPoint;
-    this.alive    = true;
-    this.stage    = args.stage;
-    this.clock    = new Clock(this);
-    this.shape    = null;
-    this.hitArea  = null;
-    this.text     = null;
-    this.p        = new Promise((resolve) => {
+    this.alive = true;
+    this.stage = args.stage;
+    this.clock = new Clock(this);
+    this.shape = null;
+    this.hitArea = null;
+    this.text = null;
+    console.log(`enemy_${this.id} spawned`);
+    this.p = new Promise((resolve) => {
       EventWrapper.on(`enemyDestroyed_${this.id}`, () => {
         EventWrapper.removeAllListeners(`enemyDestroyed_${this.id}`);
         resolve();
@@ -46,25 +47,25 @@ class Enemy {
     });
     this._assignTickListener();
   }
-  
-  addInstance(){
+
+  addInstance() {
     Enemy.instances = Enemy.instances || [];
     Enemy.instances.push(this);
   }
-  
+
   updatePos(x = this.x, y = this.y) {
     if (!this.alive) {
       return;
     }
-    
-    this.shape.x   = x;
-    this.shape.y   = y;
+
+    this.shape.x = x;
+    this.shape.y = y;
     this.hitArea.x = x;
     this.hitArea.y = y;
-    this.text.x    = x;
-    this.text.y    = y;
+    this.text.x = x;
+    this.text.y = y;
   }
-  
+
   _assignTickListener() {
     this.clock.onTick(() => {
       if (!this.alive) {
@@ -73,93 +74,93 @@ class Enemy {
       this.trigger();
       this.move();
       let gatlingBullets = ((FriendBullet.instances || {}).GatlingBullet || []);
-        this.collisionCheck(gatlingBullets);
+      this.collisionCheck(gatlingBullets);
     });
   }
-  
+
   /**
    * collision test with your bullets.
    * @param {Array<Object>} targetArray - Object must have #shape to hitTest
    */
   collisionCheck(targetArray) {
-    
+
     for (let i = 0; i < targetArray.length; i++) {
       if (!this.alive) {
         break;
       }
-      let target  = targetArray[i];
-      let pos     = this.hitArea.localToLocal(0, 0, target.shape);
+      let target = targetArray[i];
+      let pos = this.hitArea.localToLocal(0, 0, target.shape);
       let hitTest = this.hitArea.hitTest(pos.x, pos.y);
       if (hitTest) {
-        this.beShot({x: pos.x, y: pos.y, bullet: target});
+        this.beShot({ x: pos.x, y: pos.y, bullet: target });
       }
     }
   }
-  
+
   /**
    * kicked by collision check.
    *
    * @param args
    */
   beShot(args) {
-  
+
     if (!this.alive) {
       return;
     }
-  
+
     let x = args.x;
     let y = args.y;
-    
+
     new Blow({
       stage: this.stage,
-      x    : this.x - x,
-      y    : this.y - y,
+      x: this.x - x,
+      y: this.y - y,
       color: 'red',
     });
-  
+
     this.beHit(args);
     args.bullet.die();
   }
-  
+
   trigger() {
     throw new Error('implement abstract #trigger.');
   }
-  
+
   move() {
     throw new Error('implement abstract #move.');
   }
-  
+
   beHit(args) {
     let bullet = args.bullet;
-    
+
     this.hitPoint -= bullet.stoppingPower;
   }
-  
+
   destroyed() {
-    
+
     new Blow({
-      stage : this.stage,
-      x     : this.x,
-      y     : this.y,
-      color : 'red',
+      stage: this.stage,
+      x: this.x,
+      y: this.y,
+      color: 'red',
       radius: 100
     });
-    
+
     this.die();
   }
-  
+
   die() {
     this.stage.removeChild(this.shape);
     this.stage.removeChild(this.text);
     this.clock.allOff();
     this.airCraft = null;
-    this.clock    = null;
-    this.shape    = null;
-    this.text     = null;
-    this.hitArea  = null;
-    this.stage    = null;
+    this.clock = null;
+    this.shape = null;
+    this.text = null;
+    this.hitArea = null;
+    this.stage = null;
     EventWrapper.emit(`enemyDestroyed_${this.id}`);
-  
+
     for (let i = 0; i < Enemy.instances.length; i++) {
       let enemy = Enemy.instances[i];
       if (enemy === this) {
@@ -168,10 +169,10 @@ class Enemy {
       }
     }
   }
-  
+
 }
 
-Enemy.id        = 0;
+Enemy.id = 0;
 Enemy.instances = [];
 
 export default Enemy;
--- src/class/Wave.js
+++ src/class/Wave.js
@@ -1,29 +1,86 @@
 "use strict";
 import Battery from "./Battery";
 import EventsWrapper from "./EventsWrapper";
+import AirCraft from "./AirCraft";
 
 class Wave {
   constructor(index, stage) {
-    this.promises = [];
-    this.index    = index;
-    this.stage    = stage;
-    this.summonBattery(650);
-    this.summonBattery(660);
-    this.summonBattery(670);
+    this.index = index;
+    this.stage = stage;
+
+    /*
+     * dispatch enemy waves from config.
+     */
+    this.promises = this.comeOut();
+
     this.p = Promise.all(this.promises)
       .then(() => {
+        console.warn('wave clear!!');
         EventsWrapper.emit('wave.Clear');
       })
   }
-  
-  summonBattery(x, y) {
-    let b = new Battery({
-      x       : x || 600,
-      y       : y || -50,
-      hitPoint: 1,
-      stage   : this.stage,
-    });
-    this.promises.push(b.p);
+
+  /**
+   * @return {Array<Promise>}
+   */
+  comeOut() {
+
+    let promiseArray = [];
+    let waveConf = [
+      {
+        enemyClass: Battery,
+        delayMs: 0,
+        args: {
+          x: 600,
+          y: -50,
+          hitPoint: 1,
+        }
+      },
+      {
+        enemyClass: Battery,
+        delayMs: 1000,
+        args: {
+          x: 600,
+          y: -50,
+          hitPoint: 1,
+        }
+      }, {
+        enemyClass: Battery,
+        delayMs: 2000,
+        args: {
+          x: 600,
+          y: -50,
+          hitPoint: 1,
+        }
+      }
+    ];
+    waveConf.forEach((w) => {
+      let promise = this.deployEnemy(w);
+      promiseArray.push(promise);
+    })
+
+    return promiseArray;
+  }
+
+  /**
+   * @param {Object}
+   * @return {Promise}
+   */
+  deployEnemy(config) {
+
+    let enemyClass = config.enemyClass;
+    let delayMs = config.delayMs
+    let args = config.args;
+    args.stage = this.stage;
+    return new Promise((resolve, reject) => {
+      window.setTimeout(() => {
+        let e = new enemyClass(args);
+        let p = e.p;
+        p.then(() => {
+          resolve();
+        })
+      }, delayMs);
+    })
   }
 }
 
